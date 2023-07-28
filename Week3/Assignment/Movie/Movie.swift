caa94369a9b36fc0ce6b7d562026fb8b68869f4b
YesCoach <sbsii1020@gmail.com>
2023-07-28 16:26:01 +0900

    feat: Movie 스토리보드와 모델, ViewController 추가

---
+++ Week3/Assignment/Movie/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  Week3
+//
+//  Created by 박태현 on 2023/07/28.
+//
+
+import Foundation
+
+struct Movie {
+    var title: String
+    var releaseDate: Date
+    var summary: String
+    var grade: Double
+}
---
+++ Week3/Assignment/Movie/MovieListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MovieListViewController.swift
+//  Week3
+//
+//  Created by 박태현 on 2023/07/28.
+//
+
+import UIKit
+
+final class MovieListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
